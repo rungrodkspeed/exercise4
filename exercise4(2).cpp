c04faa3a20715316d2bc2d5d8348eb9a5ca270cc
rungrodkspeed <46591181+rungrodkspeed@users.noreply.github.com>
2019-01-18 14:06:15 +0700

    Update exercise4(2).cpp

--- exercise4(2).cpp
+++ exercise4(2).cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int greatDivisor(int x)
 {
-    int i=2;
-    while(i<x && x%i != 0){
-            i++;
-        }
-    return i;
+	int i=x-1;
+	if(x == 1){
+		return x;
+    }else{
+    	while(x%i !=0){
+    		i--;
+		}
+		return i;
+	}
 }
 
 int main(){
-    cout << greatDivisor(49);
+	cout << greatDivisor(93);
 }
+	
